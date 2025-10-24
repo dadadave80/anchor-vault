0fbb94d7a4d096c83a77d892c119dcb6239ee46a
David Dada <daveproxy80@gmail.com>
2025-10-24 23:43:18 +0100

    Complete anchor vault

--- programs/anchor-vault-q4-25/src/lib.rs
+++ programs/anchor-vault-q4-25/src/lib.rs
@@ -1,5 +1,5 @@
 use anchor_lang::{
-    prelude::*,
+    prelude::{bpf_loader_upgradeable::close, *},
     system_program::{transfer, Transfer},
 };
 
@@ -13,17 +13,17 @@ pub mod anchor_vault_q4_25 {
         ctx.accounts.initialize(&ctx.bumps)
     }
 
-    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+    pub fn deposit(ctx: Context<Transact>, amount: u64) -> Result<()> {
         ctx.accounts.deposit(amount)
     }
 
-    // pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
-    //     ctx.accounts.withdraw(amount)
-    // }
+    pub fn withdraw(ctx: Context<Transact>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)
+    }
 
-    // pub fn close(ctx: Context<Close>) -> Result<()> {
-    //     ctx.accounts.close()
-    // }
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()
+    }
 }
 
 #[derive(Accounts)]
@@ -71,7 +71,7 @@ impl<'info> Initialize<'info> {
 }
 
 #[derive(Accounts)]
-pub struct Deposit<'info> {
+pub struct Transact<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
     #[account(
@@ -88,7 +88,7 @@ pub struct Deposit<'info> {
     pub system_program: Program<'info, System>,
 }
 
-impl<'info> Deposit<'info> {
+impl<'info> Transact<'info> {
     pub fn deposit(&mut self, amount: u64) -> Result<()> {
         let cpi_program = self.system_program.to_account_info();
 
@@ -103,32 +103,73 @@ impl<'info> Deposit<'info> {
 
         Ok(())
     }
+
+    pub fn withdraw(&mut self, amount: u64) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let seed: &[&[&[u8]]] = &[&[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seed);
+
+        transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
 }
 
-// #[derive(Accounts)]
-// pub struct Withdraw<'info> {
-// TODO: Implement Withdraw accounts
-// }
+#[derive(Accounts)]
+pub struct Close<'info> {
+    // TODO: Implement Close accounts
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+         mut,
+         seeds = [b"vault", vault_state.key().as_ref()], 
+         bump = vault_state.vault_bump,
+     )]
+    pub vault: SystemAccount<'info>,
+    #[account(
+        mut,
+         seeds = [b"state", user.key().as_ref()],
+         bump = vault_state.state_bump,
+         close = user,
+     )]
+    pub vault_state: Account<'info, VaultState>,
+    pub system_program: Program<'info, System>,
+}
 
-// impl<'info> Withdraw<'info> {
-//     pub fn withdraw(&mut self, _amount: u64) -> Result<()> {
-//         TODO: Implement withdraw
+impl<'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        //  TODO: Implement close
+        let cpi_program = self.system_program.to_account_info();
 
-//         Ok(())
-//     }
-// }
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
 
-// #[derive(Accounts)]
-// pub struct Close<'info> {
-//      TODO: Implement Close accounts
-// }
+        let seed: &[&[&[u8]]] = &[&[
+            b"vault",
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ]];
 
-// impl<'info> Close<'info> {
-//     pub fn close(&mut self) -> Result<()> {
-//          TODO: Implement close
-//         Ok(())
-//     }
-// }
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, seed);
+
+        transfer(cpi_ctx, self.vault.lamports())?;
+
+        Ok(())
+    }
+}
 
 #[derive(InitSpace)]
 #[account]
